1b2116b7586dd8caddd98b0250995ae08bb2e02b
samikshanimbokar5-crypto <samikshanimbokar5@gmail.com>
2026-09-06 14:25:17 +0530

    Support Railway MySQL variables

--- config.example.php
+++ config.example.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'host' => getenv('IDEAVAULT_DB_HOST') ?: '127.0.0.1',
-    'port' => getenv('IDEAVAULT_DB_PORT') ?: '3306',
-    'database' => getenv('IDEAVAULT_DB_NAME') ?: 'ideavault_db',
-    'username' => getenv('IDEAVAULT_DB_USER') ?: 'root',
-    'password' => getenv('IDEAVAULT_DB_PASSWORD') ?: '',
+    'host' => getenv('IDEAVAULT_DB_HOST') ?: getenv('MYSQLHOST') ?: '127.0.0.1',
+    'port' => getenv('IDEAVAULT_DB_PORT') ?: getenv('MYSQLPORT') ?: '3306',
+    'database' => getenv('IDEAVAULT_DB_NAME') ?: getenv('MYSQLDATABASE') ?: 'ideavault_db',
+    'username' => getenv('IDEAVAULT_DB_USER') ?: getenv('MYSQLUSER') ?: 'root',
+    'password' => getenv('IDEAVAULT_DB_PASSWORD') ?: getenv('MYSQLPASSWORD') ?: '',
 ];
