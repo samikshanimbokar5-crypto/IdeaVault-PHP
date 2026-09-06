6cbfedf9f75825573320e5b3df8d8dea8b4b501b
samikshanimbokar5-crypto <samikshanimbokar5@gmail.com>
2026-09-06 14:27:18 +0530

    Support underscore MySQL variables

--- config.example.php
+++ config.example.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'host' => getenv('IDEAVAULT_DB_HOST') ?: getenv('MYSQLHOST') ?: '127.0.0.1',
-    'port' => getenv('IDEAVAULT_DB_PORT') ?: getenv('MYSQLPORT') ?: '3306',
-    'database' => getenv('IDEAVAULT_DB_NAME') ?: getenv('MYSQLDATABASE') ?: 'ideavault_db',
-    'username' => getenv('IDEAVAULT_DB_USER') ?: getenv('MYSQLUSER') ?: 'root',
-    'password' => getenv('IDEAVAULT_DB_PASSWORD') ?: getenv('MYSQLPASSWORD') ?: '',
+    'host' => getenv('IDEAVAULT_DB_HOST') ?: getenv('MYSQLHOST') ?: getenv('MYSQL_HOST') ?: '127.0.0.1',
+    'port' => getenv('IDEAVAULT_DB_PORT') ?: getenv('MYSQLPORT') ?: getenv('MYSQL_PORT') ?: '3306',
+    'database' => getenv('IDEAVAULT_DB_NAME') ?: getenv('MYSQLDATABASE') ?: getenv('MYSQL_DATABASE') ?: 'ideavault_db',
+    'username' => getenv('IDEAVAULT_DB_USER') ?: getenv('MYSQLUSER') ?: getenv('MYSQL_USER') ?: 'root',
+    'password' => getenv('IDEAVAULT_DB_PASSWORD') ?: getenv('MYSQLPASSWORD') ?: getenv('MYSQL_PASSWORD') ?: '',
 ];
